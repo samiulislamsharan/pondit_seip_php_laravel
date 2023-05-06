5edb33fe645bb8b5d8c9d058e27f6b82465581cd
Samiul Islam <samiulislamsharan@gmail.com>
2023-05-07 01:56:46 +0600

    super global variables

---
+++ session_11/super_globals.php
@@ -0,0 +1,12 @@
+<?php
+// Superglobals — Built-in variables that are always available in all scopes
+
+// $GLOBALS — References all variables available in global scope
+// $_SERVER — Server and execution environment information
+// $_GET — HTTP GET variables
+// $_POST — HTTP POST variables
+// $_FILES — HTTP File Upload variables
+// $_REQUEST — HTTP Request variables
+// $_SESSION — Session variables
+// $_ENV — Environment variables
+// $_COOKIE — HTTP Cookies
